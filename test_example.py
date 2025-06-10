0f290b259c0bbe8ffd5a799163f6b7a94553facb
Alexander QA <112622680+qartavtsev-qameta@users.noreply.github.com>
2025-06-10 11:18:26 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -18,3 +18,12 @@ def test_example(username):
             pass
     with allure.step("Step 3"):
         pass
+
+@allure.title("Test case example for NBC Universal - dynamic parameter"
+def test_sum():
+    x = 1
+    y = 2
+    ttl = 3
+    allure.dynamic.parameter("x", x)
+    allure.dynamic.parameter("y", y)
+    allure.dynamic.parameter("sum", ttl)
