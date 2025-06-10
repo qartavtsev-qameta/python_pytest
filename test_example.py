752b48a2be0dc6b2027deecaabb26a7c95de5840
Alexander QA <112622680+qartavtsev-qameta@users.noreply.github.com>
2025-06-10 11:06:02 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -4,10 +4,10 @@ import pytest
 @allure.tag("Tag 1", "Tag 2", "Tag 3")
 @allure.title("Test case example for NBC Universal")
 @allure.label("owner", "John Smith")
+@allure.label("priority", "1 - High")
+@allure.label("service", "Authentication Service")
 @allure.feature("Authentication")
 def test_example():
-    Allure::label("priority", "1 - High");
-    Allure::label("service", "Authentication Service");
     with allure.step("Step 1"):
         pass
     with allure.step("Step 2"):
