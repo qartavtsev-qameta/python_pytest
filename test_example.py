28b6afc4f46ee22027b66425d81f9b0ccb0bcc78
Alexander QA <112622680+qartavtsev-qameta@users.noreply.github.com>
2025-06-10 11:02:51 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,22 +1,19 @@
 import allure
-import random
 import pytest
 
-
-@allure.epic("Authentication and Access Management")
-@allure.feature("Authentication")
-@allure.story("Create new issue")
-@allure.tag("Tag")
-@allure.label("severity", "1 - High")
-@allure.label("service", "Monitoring Service")
+@allure.tag("Tag 1", "Tag 2", "Tag 3")
+@allure.title("Test case example for NBC Universal")
 @allure.label("owner", "John Smith")
-@pytest.mark.parametrize("random_fail", [random.choice([True, False])])
-def test_example(random_fail):
-    allure.step("Step 1")
-    
+@allure.feature("Authentication")
+def test_example():
+    Allure::label("priority", "1 - High");
+    Allure::label("service", "Authentication Service");
+    with allure.step("Step 1"):
+        pass
     with allure.step("Step 2"):
-        allure.step("Sub-step 2.1")
+        with allure.step("Sub-step 2.1"):
+            pass
         with allure.step("Sub-step 2.2"):
-            allure.step("Sub-step 2.2.1")
-                if random_fail:
-                    pytest.fail("Step failed!")
+            pass
+    with allure.step("Step 3"):
+        pass
